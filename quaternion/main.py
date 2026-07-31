39af75882a3b1371b24bb1cc1cec07715bc50aa0
ep1n <albertcjaworski@gmail.com>
2026-07-31 16:48:58 -0400

    fsdafsdafsda

--- quaternion/main.py
+++ quaternion/main.py
@@ -3,25 +3,58 @@ from manim_combinable import *
 
 class IntroIntro(ThreeDScene):
     def construct(self):
+        world_x = Arrow3D(start=ORIGIN, end=RIGHT).set_color(RED)
+        world_y = Arrow3D(start=ORIGIN, end=UP).set_color(GREEN)
+        world_z = Arrow3D(start=ORIGIN, end=np.array([0,0,1])).set_color(BLUE)
+        anchor = VGroup(world_x,world_y,world_z).set_opacity(0.0)
+        self.add(anchor)
         self.wait(1)
-        torusX = Torus(major_radius=1, minor_radius=.05, stroke_width=0).set_color(color.BLUE_C).rotate(90*DEGREES, np.array([1, 0, 0]))
-        torusY = Torus(major_radius=1.12, minor_radius=.05, stroke_width=0).set_color(color.RED_C)
-        torusZ = Torus(major_radius=1.24, minor_radius=.05, stroke_width=0).set_color(color.GREEN_C).rotate(90*DEGREES, np.array([0, 1, 0]))
-        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
-        self.camera.set_zoom(.9)
+        light = self.camera.light_source
+        light.shift(UP*20)
+        torusX = Torus(major_radius=1, minor_radius=.05, stroke_width=0).set_color(color.RED_C).rotate(90*DEGREES, np.array([0, 1, 0]))
+        xcon1 = Cylinder(radius=.04, height=.1, direction=UP)
+        xcon2 = Cylinder(radius=.04, height=.1, direction=UP)
+        localX = Arrow3D(start=ORIGIN, end=RIGHT).set_color(RED_C)
+        torusY = Torus(major_radius=1.12, minor_radius=.05, stroke_width=0).set_color(color.GREEN_C).rotate(90*DEGREES, np.array([1, 0, 0]))
+        ycon1
+        ycon2 
+        localY = Arrow3D(start=ORIGIN, end=UP).set_color(GREEN_C)
+        torusZ = Torus(major_radius=1.24, minor_radius=.05, stroke_width=0).set_color(color.BLUE_C)
+        zcon1
+        zcon2
+        localZ = Arrow3D(start=ORIGIN, end=np.array([0,0,1])).set_color(BLUE_C)
+        gimbal = VGroup(torusX, torusY, torusZ)
+        core = VGroup(localX, localY, localZ).set_opacity(1.0)
+        self.add(core)
+        rig = VGroup(gimbal, core).rotate(20*DEGREES, RIGHT).rotate(-10*DEGREES, localY.get_end())
+        self.camera.set_zoom(.7)
         self.play(AnimationGroup(
-            FadeIn(torusX),
-            FadeIn(torusY),
-            FadeIn(torusZ)
+            FadeIn(gimbal),
         ), AnimationGroup(
-            Rotate(torusX, 360*DEGREES, np.array([1,2,3])),
-            Rotate(torusY, 360*DEGREES, np.array([1,2,3])),
-            Rotate(torusZ, 360*DEGREES, np.array([1,2,3])),
+            Rotate(gimbal, 360*DEGREES, np.array([1,2,3])),
             rate_func=rate_functions.ease_out_cubic
         ),
         run_time=2
         )
         self.wait(2)
+        self.play(AnimationGroup(
+            #x, z, y
+            Rotate(gimbal, 180*DEGREES, localZ.get_end()),
+            Rotate(gimbal, 180*DEGREES, localY.get_end()),
+            Rotate(gimbal, 180*DEGREES, localX.get_end()),  
+            lag_ratio=1.4,
+            run_time=3
+        ))
+        self.wait(5)    
+        GLock = Text("Gimbal Lock").shift(UP*1.5).scale(.7)
+        self.play(AnimationGroup(
+            rig.animate.shift(DOWN*.5),
+            Write(GLock),
+            lag_ratio=.5,
+            run_time=2
+        ))
+        self.wait(3)
+
 
 class Transition(Scene):
     def construct(self):

017b16fd0ba10d0a871230b5c7286c88722ad76c
BuildTools <unconfigured@null.spigotmc.org>
2026-08-12 23:01:55 -0400

    RAHSDOFUJHBSD:VKJHGSDFLKDSJFS:KVJGSD>VKHSDGFKJSDHBFLSDHGVLJSDBVDSF I AM RULER I AM RULER I AM RULER I AM RULER I AM RULER I AM RULER

--- quaternion/main.py
+++ quaternion/main.py
@@ -1,8 +1,10 @@
 from manim import *
 from manim_combinable import *
+import math
 
 class IntroIntro(ThreeDScene):
     def construct(self):
+        self.camera.set_zoom(.8)
         world_x = Arrow3D(start=ORIGIN, end=RIGHT).set_color(RED) # pyright: ignore[reportUndefinedVariable]
         world_y = Arrow3D(start=ORIGIN, end=UP).set_color(GREEN)
         world_z = Arrow3D(start=ORIGIN, end=np.array([0,0,1])).set_color(BLUE)
@@ -10,25 +12,32 @@ class IntroIntro(ThreeDScene):
         self.add(anchor)
         self.wait(1)
         light = self.camera.light_source
-        light.shift(UP*20)
-        torusX = Torus(major_radius=1, minor_radius=.05, stroke_width=0).set_color(color.RED_C).rotate(90*DEGREES, np.array([0, 1, 0]))
-        xcon1 = Cylinder(radius=.04, height=.1, direction=UP).shift(IN*1.05).set_color(color.RED_C)
-        xcon2 = Cylinder(radius=.04, height=.1, direction=UP).shift(OUT*1.05).set_color(color.RED_C)
-        localX = Arrow3D(start=ORIGIN, end=RIGHT).set_color(RED_C)
-        torusY = Torus(major_radius=1.12, minor_radius=.05, stroke_width=0).set_color(color.GREEN_C).rotate(90*DEGREES, np.array([1, 0, 0]))
-        ycon1 = Cylinder(radius=.04, height=.1, direction=RIGHT).shift(RIGHT*1.17).set_color(color.GREEN_C)
-        ycon2 = Cylinder(radius=.04, height=.1, direction=RIGHT).shift(LEFT*1.17).set_color(color.GREEN_C)
-        localY = Arrow3D(start=ORIGIN, end=UP).set_color(GREEN_C)
-        torusZ = Torus(major_radius=1.24, minor_radius=.05, stroke_width=0).set_color(color.BLUE_C)
-        zcon1 = Cylinder(radius=.04, height=.1, direction=UP).shift(UP*1.29).set_color(color.BLUE_C)
-        zcon2 = Cylinder(radius=.04, height=.1, direction=UP).shift(DOWN*1.29).set_color(color.BLUE_C)
-        localZ = Arrow3D(start=ORIGIN, end=np.array([0,0,1])).set_color(BLUE_C)
-        linkages = VGroup(xcon1, xcon2, ycon1, ycon2, zcon1, zcon2).set_opacity(0.0)
-        gimbal = VGroup(torusX, torusY, torusZ, linkages)
+        light.shift(UP*40)
+        torusX = Torus(major_radius=3, minor_radius=.15, stroke_width=0).set_color(color.RED).rotate(90*DEGREES, np.array([0, 1, 0]))
+        xcon1 = Cylinder(radius=.12, height=.3, direction=UP).shift(IN*3.15).set_color(color.RED)
+        xcon2 = Cylinder(radius=.12, height=.3, direction=UP).shift(OUT*3.15).set_color(color.RED)
+        localX = Arrow3D(start=ORIGIN, end=RIGHT).set_color(RED)
+        rigX = VGroup(torusX, xcon1, xcon2)
+        torusY = Torus(major_radius=3.36, minor_radius=.15, stroke_width=0).set_color(color.GREEN).rotate(90*DEGREES, np.array([1, 0, 0]))
+        ycon1 = Cylinder(radius=.12, height=.3, direction=RIGHT).shift(RIGHT*3.51).set_color(color.GREEN)
+        ycon2 = Cylinder(radius=.12, height=.3, direction=RIGHT).shift(LEFT*3.51).set_color(color.GREEN)
+        localY = Arrow3D(start=ORIGIN, end=UP).set_color(GREEN)
+        rigY = VGroup(torusY, ycon1, ycon2)
+        torusZ = Torus(major_radius=3.72, minor_radius=.15, stroke_width=0).set_color(color.BLUE)
+        zcon1 = Cylinder(radius=.12, height=.3, direction=UP).shift(UP*3.87).set_color(color.BLUE)
+        zcon2 = Cylinder(radius=.12, height=.3, direction=UP).shift(DOWN*3.87).set_color(color.BLUE)
+        rigZ = VGroup(torusZ, zcon1, zcon2)
+        localZ = Arrow3D(start=ORIGIN, end=np.array([0,0,1])).set_color(BLUE)
+        gimbal = VGroup(rigX, rigY, rigZ)
         core = VGroup(localX, localY, localZ).set_opacity(0.0)
         self.add(core)
         rig = VGroup(gimbal, core).rotate(20*DEGREES, RIGHT).rotate(-10*DEGREES, localY.get_end())
-        self.camera.set_zoom(.7)
+        xcon1.set_opacity(0.0),
+        xcon2.set_opacity(0.0),
+        ycon1.set_opacity(0.0),
+        ycon2.set_opacity(0.0),
+        zcon1.set_opacity(0.0),
+        zcon2.set_opacity(0.0)
         self.play(AnimationGroup(
             FadeIn(gimbal),
         ), AnimationGroup(
@@ -46,10 +55,10 @@ class IntroIntro(ThreeDScene):
             lag_ratio=1.4,
             run_time=3
         ))
-        self.wait(5)    
-        GLock = Text("Gimbal Lock").shift(UP*1.5).scale(.7)
+        self.wait(2)    
+        GLock = Text("Gimbal Lock").shift(UP*4).scale(1.5)
         self.play(AnimationGroup(
-            rig.animate.shift(DOWN*.5),
+            gimbal.animate.shift(DOWN*.75),
             Write(GLock),
             lag_ratio=.5,
             run_time=2
@@ -57,16 +66,367 @@ class IntroIntro(ThreeDScene):
         self.wait(3)
         self.play(AnimationGroup(
             Unwrite(GLock),
-            rig.animate.shift(UP*.5),
+            gimbal.animate.shift(UP*.75), #the numbers were off on the previous rig shift so all rotations were messed up slightly (i think)
             lag_ratio=.5,
             run_time=2
         ))
         self.play(
-            linkages.animate.set_opacity(1.0)
+            xcon1.animate.set_opacity(1.0),
+            xcon2.animate.set_opacity(1.0),
+            ycon1.animate.set_opacity(1.0),
+            ycon2.animate.set_opacity(1.0),
+            zcon1.animate.set_opacity(1.0),
+            zcon2.animate.set_opacity(1.0)
         )
         self.wait(3)
+        self.play(
+            Rotate(rigX, 90*DEGREES, localX.get_end()),
+            Rotate(torusY, 90*DEGREES, localX.get_end()),
+        )
+        self.play(
+            Rotate(torusX, 90*DEGREES, localY.get_end())
+        )
+        self.wait(3)
+        self.play(gimbal.animate.shift(DOWN*.75))
+        text = Text("Rotating about z-axis", t2c={"z": BLUE_C}).shift(UP*4).scale(1.5)
+        self.play(Write(text))
+        self.play(
+            Rotate(rigY, 180*DEGREES, localY.get_end()),
+            Rotate(rigX, 180*DEGREES, localY.get_end()),
+            Rotate(rigZ, 180*DEGREES, localY.get_end())
+        )
+        self.wait(1)
+        tex2 = Text("Rotating about x-axis", t2c={" x": RED_C}).shift(UP*4).scale(1.5)
+        self.play(
+            TransformMatchingShapes(text, tex2)
+        )
+        self.wait(1)
+        self.play(
+            Rotate(rigX, 180*DEGREES, localY.get_end())
+        )
+        self.wait(2)
+        self.play(
+            Unwrite(tex2)
+        )
+        self.play(
+            gimbal.animate.shift(UP*.75)
+        )
+        self.play(
+            Rotate(rigX, 30*DEGREES, localX.get_end()),
+            Rotate(torusY, 30*DEGREES, localX.get_end()),
+        )
+        self.wait(1)
+        self.play(
+            Rotate(rigX, -30*DEGREES, localX.get_end()),
+            Rotate(torusY, -30*DEGREES, localX.get_end()),
+        )
+        self.wait(1)
+        localZ.set_color(RED).scale(3)
+        self.play(
+            localZ.animate.set_opacity(1)
+        )
+        self.wait(2)
+        self.play(
+            Uncreate(localZ)
+        )
+        self.wait(2)
+        self.play(
+            Uncreate(gimbal)
+        )
 
+class Imaginary(Scene):
+    def construct(self):
+        imag = MathTex("i=\\sqrt{-1}")
+        self.play(
+            Write(imag)
+        )
+        self.wait(1)
+        eq = MathTex("x^{2}+1=0")
+        self.play(
+            AnimationGroup(imag.animate.shift(UP*1.5)),
+            Write(eq),
+            lag_ratio=.4
+        )
+        self.wait(1)
+        sol = MathTex("x=i,-i")
+        self.play(
+            AnimationGroup(eq.animate.shift(DOWN*1.5)),
+            Write(sol),
+            lag_ratio=.4
+        )
+        self.wait(2)
+        self.play(
+            Unwrite(imag),
+            Unwrite(eq),
+            Unwrite(sol)
+        )
+        std_plane = NumberPlane(
+            background_line_style={
+                "stroke_color": GREY,
+                "stroke_width": 4,
+                "stroke_opacity": 0.6
+            }
+        )
+        self.wait(1)
+        self.play(
+            Create(std_plane)
+        )
+        func1 = FunctionGraph(
+            lambda x: x**2 + 1
+        )
+        self.wait(1)
+        self.play(
+            Write(func1)
+        )
+        self.wait(2)
+        self.play(
+            FadeOut(func1),
+            FadeOut(std_plane)
+        )
+        self.wait(1)
+        tex1 = Text("Useless").shift(UP)
+        tex2 = Text("False")
+        tex3 = Text("Imaginary").set_color(YELLOW).shift(DOWN)
+        self.play(AnimationGroup(
+            Write(tex1),
+            Write(tex2),
+            Write(tex3),
+            lag_ratio=1,
+            run_time=4.5
+        ))
+        self.wait(3)
+        self.play(
+            FadeOut(tex1),
+            FadeOut(tex2),
+            FadeOut(tex3)
+        )
+        eq2 = MathTex("x^{3}-15x-4").shift(UP*2)
+        key2 = MathTex("x^3+px+q").shift(UP*1.25)
+        eq3 = MathTex("u_1=-\\frac{q}{2}+\\sqrt{\\frac{q^2}{4}+\\frac{p^3}{27}}").shift(LEFT*3)
+        eq4 = MathTex("u_2=-\\frac{q}{2}-\\sqrt{\\frac{q^2}{4}+\\frac{p^3}{27}}").shift(RIGHT*3)
+        eq5 = MathTex("x=\\sqrt[3]{u_1}+\\sqrt[3]{u_2}").shift(DOWN*1.25)
+        self.wait(2)
+        self.play(AnimationGroup(
+            Write(eq2),
+            Write(key2),
+            Write(eq3),
+            Write(eq4),
+            Write(eq5),
+            lag_ratio=.3
+        ))
+        self.wait(5)
+        self.play(
+            eq2.animate.set_color(RED)
+        )
+        self.wait(1)
+        self.play(
+            eq3.animate.set_color(RED),
+            eq4.animate.set_color(RED)
+        )
+        self.wait(2)
+        self.play(
+            FadeOut(eq2),
+            FadeOut(key2),
+            FadeOut(eq3),
+            FadeOut(eq4),
+            FadeOut(eq5)
+        )
+        self.remove(eq2)
+        self.wait(2)
+        eq2.shift(DOWN*1.5)
+        eq6 = MathTex("{{(4)^3}}-{{15(4)}}-{{(4)}}").shift(UP*.5)
+        eq65 = MathTex("{{64}}-{{60}}-{{4}}").shift(UP*.5)
+        eq675 = MathTex("{{64}}-{{64}}=0").shift(UP*.5)
+        eq7 = MathTex("x^3+2x-4").shift(DOWN*.5).shift(LEFT*1.5)
+        self.play(
+            Write(eq2)
+        )
+        self.wait(.5)
+        self.play(
+            TransformMatchingTex(eq2, eq6)
+        )
+        self.wait(.5)
+        self.play(
+            TransformMatchingTex(eq6, eq65)
+        )
+        self.wait(.5)
+        self.play(
+            TransformMatchingTex(eq65, eq675)
+        )
+        self.wait(2)
+        self.play(
+            Write(eq7)
+        )
+        eq8 = MathTex("x=2").shift(DOWN*.5).shift(RIGHT*1.5)
+        self.wait(2)
+        self.play(
+            Write(eq8)
+        )
+        self.wait(2)
+        self.play(
+            Unwrite(eq675),
+            Unwrite(eq7),
+            Unwrite(eq8)
+        )
+        self.wait(2)
+        line = NumberLine(
+            x_range=[-5,5,1],
+            length=20, #should be going off-screen
+            color=WHITE,
+            include_numbers=True
+        ).center()
+        line.shift(-line.n2p(0))
+        self.play(
+            Create(line),
+        )
+        ind = Arrow(start=UP, end=DOWN).shift(UP*1.25+RIGHT)
+        self.wait(1)
+        self.play(
+            Create(ind)
+        )
+        self.play(
+            ind.animate.shift(LEFT*4)
+        )
+        self.play(
+            ind.animate.shift(RIGHT*16)
+        )
+        self.remove(ind)
+        eq9 = MathTex("i=\\sqrt{-1}").shift(UP*2)
+        self.play(
+            Write(eq9)
+        )
+        self.wait(1)
+        self.play(Unwrite(eq9))
+        self.wait(1)
+        self.play(
+            line.animate.flip(),
+        )
+        self.wait(2)
+        self.play(
+            line.animate.flip(),
+        )
+        self.wait(2)
+        self.play(
+            Rotate(line),
+        )
+        self.wait(2)
+        self.play(
+            Rotate(line),
+        )
+        self.wait(3)
+        imagLine = NumberLine(
+            x_range=[-5,5,1],
+            length=20, #should be going off-screen
+            color=GOLD_A,
+            include_numbers=True,
+            decimal_number_config={"unit":"i", "num_decimal_places":0},
+            numbers_to_exclude=[0]
+        ).center()
+        imagLine.shift(-imagLine.n2p(0))
+        for n in imagLine.numbers:
+            n.rotate(-90*DEGREES) #check if it doesn't just send all of the numbers flying off and rotates locally
+            #it fcuking worked i'm amazing
+        self.play(
+            FadeIn(imagLine),
+            imagLine.animate.rotate_about_zero(90*DEGREES),
+            FadeOut(line.get_number_mobject(0)),
+            run_time=4,
+            rate_func=rate_functions.rush_into
+        )
+        line.set_z_index(2)
+        imagLine.set_z_index(1)
+        for i in range(1,5,1):
+            #pos neg both axis
+            self.play(
+                Create(Line(line.n2p(i)+5*UP,line.n2p(i)+5*DOWN).set_color(DARK_GREY)),
+                Create(Line(line.n2p(-i)+5*UP,line.n2p(-i)+5*DOWN).set_color(DARK_GREY)),
+                Create(Line(imagLine.n2p(i)+8*RIGHT,imagLine.n2p(i)+8*LEFT).set_color(DARK_GREY)),
+                Create(Line(imagLine.n2p(-i)+8*RIGHT,imagLine.n2p(-i)+8*LEFT).set_color(DARK_GREY)),
+            )
+        self.wait(2)
+        onei = CurvedArrow(line.n2p(1)+UP*.2, imagLine.n2p(1)+RIGHT*.2)
+        ineg = CurvedArrow(imagLine.n2p(1)+LEFT*.2, line.n2p(-1)+UP*.2)
+        negi = CurvedArrow(line.n2p(-1)+DOWN*.2, imagLine.n2p(-1)+LEFT*.2)
+        ione = CurvedArrow(imagLine.n2p(-1)+RIGHT*.2, line.n2p(1)+DOWN*.2)
+        self.play(
+            Write(onei),
+            Write(ineg),
+            Write(negi),
+            Write(ione),
+            run_time=6,
+            lag_ratio=1.5
+        )
+        self.wait(2)
+        self.play(
+            Indicate(onei),
+            Indicate(ineg)
+        )
+        self.wait(1)
+        self.play(
+            Indicate(onei)
+        )
+        self.wait(1)
+        self.play(AnimationGroup(
+            Indicate(onei),
+            Indicate(ineg),
+            lag_ratio=.5,
+            run_time=2
+        ))
+        self.wait(2)
+        self.play(
+            Unwrite(onei),
+            Unwrite(ineg),
+            Unwrite(negi),
+            Unwrite(ione)
+        )
+        circ = Circle(radius=2.0)
+        self.play(
+            Write(circ)
+        )
+        self.wait(2)
+        lline = Line(line.n2p(0), line.n2p(5)).set_color(RED).shift(OUT*.01)
+        self.add(lline)
+        self.play(
+            lline.animate.rotate_about_origin(45*DEGREES)
+        )
+        self.wait(1)
+        coord = 1/math.sqrt(2)
+        dot = Dot().set_color(BLUE).shift(coord*UP*2+coord*RIGHT*2)
+        coords = MathTex("(\\frac{1}{\\sqrt{2}},\\frac{1}{\\sqrt{2}})").move_to(dot).shift(RIGHT*1.5).set_color(BLUE)
+        self.play(
+            Write(coords),
+            Create(dot)
+        )
+        degrees = MathTex("45^{\\circ}").set_color(RED).move_to(dot).shift(DOWN*1+RIGHT*1.3)
+        self.play(
+            Write(degrees)
+        )
+        square = Rectangle(GOLD_A).set_fill(BLACK, 1.0).set_z_index(3)
+        eq10 = MathTex("\\cos(\\theta)+i\\sin(\\theta)").set_z_index(4)
+        self.play(AnimationGroup(
+            FadeIn(square),
+            Write(eq10),
+            run_time=2,
+            lag_ratio=.5
+        ))
+        self.wait(5)
 
+class Fourth(Scene):
+    def construct(self):
+        return super().construct()
+
+class FourthRotate(Scene):
+    def construct(self):
+        return super().construct()
+
+class Quaternions(Scene):
+    def construct(self):
+        return super().construct()
+
+class QuaternionRotation(Scene):
+    def construct(self):
+        return super().construct()
+    
 class Transition(Scene):
     def construct(self):
         self.wait(1)
